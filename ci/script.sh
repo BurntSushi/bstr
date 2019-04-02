9d9d1e29e864e913bb888fad4654e5b7ec7bb9ee
Andrew Gallant <jamslam@gmail.com>
2019-04-01 20:59:56 -0400

    ci: test benchmarks

--- ci/script.sh
+++ ci/script.sh
@@ -5,3 +5,6 @@ set -ex
 cargo build --verbose
 cargo doc --verbose
 cargo test --verbose
+if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
+  cargo bench --verbose --manifest-path bench/Cargo.toml -- --test
+fi
