dc8f855e58754ca972bfefed27adf927c6aac134
Andrew Gallant <jamslam@gmail.com>
2019-06-09 18:57:44 -0400

    ci: don't test on MSRV

--- ci/script.sh
+++ ci/script.sh
@@ -4,6 +4,13 @@ set -ex
 
 cargo build --verbose
 cargo doc --verbose
+
+# Our dev dependencies are increasing their MSRV more quickly then we want to,
+# so only test the basic build on non-stable/beta/nightly builds.
+if ! echo "$TRAVIS_RUST_VERSION" | grep -Eq '^[^0-9]+$'; then
+    exit 0
+fi
+
 cargo test --verbose
 if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
   cargo bench --verbose --manifest-path bench/Cargo.toml -- --test
