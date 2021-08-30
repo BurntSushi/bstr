c1708a5e30fb490838d63e8ec2145e91c4aa316f
Andres Suarez <zertosh@gmail.com>
2021-08-30 17:51:19 -0400

    api: impl Clone and Debug for Lines and LinesWithTerminator

--- src/ext_slice.rs
+++ src/ext_slice.rs
@@ -3521,6 +3521,7 @@ impl<'a> Iterator for SplitNReverse<'a> {
 /// `\n`.
 ///
 /// `'a` is the lifetime of the byte string being iterated over.
+#[derive(Clone, Debug)]
 pub struct Lines<'a> {
     it: LinesWithTerminator<'a>,
 }
@@ -3560,6 +3561,7 @@ impl<'a> Iterator for Lines<'a> {
 /// the original byte string.
 ///
 /// `'a` is the lifetime of the byte string being iterated over.
+#[derive(Clone, Debug)]
 pub struct LinesWithTerminator<'a> {
     bytes: &'a [u8],
 }
