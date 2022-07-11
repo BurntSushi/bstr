9b465d60e5f1bee0253642c281f394c81c1ccf50
Andrew Gallant <jamslam@gmail.com>
2022-07-11 11:43:51 -0400

    api: add BStr::new
    This adds a convenience constructor for building a BStr slice directly
    from anything that impls AsRef<[u8]>. This is analogous to, e.g.,
    Path::new from std.
    
    While this constructor is redundant, it is often convenient because of
    its concreteness. Alternatives rely on generics via trait impls, which
    don't always work seamlessly.
    
    Closes #110

--- src/bstr.rs
+++ src/bstr.rs
@@ -36,8 +36,31 @@ pub struct BStr {
 }
 
 impl BStr {
+    /// Directly creates a `BStr` slice from anything that can be converted
+    /// to a byte slice.
+    ///
+    /// This is very similar to the [`B`](crate::B) function, except this
+    /// returns a `&BStr` instead of a `&[u8]`.
+    ///
+    /// This is a cost-free conversion.
+    ///
+    /// # Example
+    ///
+    /// You can create `BStr`'s from byte arrays, byte slices or even string
+    /// slices:
+    ///
+    /// ```
+    /// use bstr::BStr;
+    ///
+    /// let a = BStr::new(b"abc");
+    /// let b = BStr::new(&b"abc"[..]);
+    /// let c = BStr::new("abc");
+    ///
+    /// assert_eq!(a, b);
+    /// assert_eq!(a, c);
+    /// ```
     #[inline]
-    pub(crate) fn new<B: ?Sized + AsRef<[u8]>>(bytes: &B) -> &BStr {
+    pub fn new<'a, B: ?Sized + AsRef<[u8]>>(bytes: &'a B) -> &'a BStr {
         BStr::from_bytes(bytes.as_ref())
     }
 
