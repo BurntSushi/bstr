732fc99f3844d88dc40f33d95a7bc8f3f6bd2e5b
Josh Triplett <josh@joshtriplett.org>
2025-01-02 16:03:25 +0200

    impl: fix formatting of control characters \x1a through \x1f in Debug impl
    Due to an incorrect range, control characters \x1a through \x1f get
    formatted as Unicode \u escapes rather than hex \x escapes.
    
    This commit fixes the range, adds a test and switches over to std's
    `escape_ascii` to do more of the formatting work.
    
    Without the fix, the test fails like this:
    
    ```
      left: "\"\\0\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x08\\t\\n\\x11\\x12\\r\\x14\\x15\\x16\\x17\\x18\\x19\\x1a\\x1b\\x1c\\x1d\\x1e\\x1f \\x7f\\x80\\x81\\xfe\\xff\""
     right: "\"\\0\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x08\\t\\n\\x11\\x12\\r\\x14\\x15\\x16\\x17\\x18\\x19\\u{1a}\\u{1b}\\u{1c}\\u{1d}\\u{1e}\\u{1f} \\x7f\\x80\\x81\\xfe\\xff\""
    ```
    
    PR #201

--- src/impls.rs
+++ src/impls.rs
@@ -525,6 +525,9 @@ mod bstr {
             for (s, e, ch) in self.char_indices() {
                 match ch {
                     '\0' => write!(f, "\\0")?,
+                    '\x01'..='\x7f' => {
+                        write!(f, "{}", (ch as u8).escape_ascii())?;
+                    }
                     '\u{FFFD}' => {
                         let bytes = self[s..e].as_bytes();
                         if bytes == b"\xEF\xBF\xBD" {
@@ -534,17 +537,6 @@ mod bstr {
                                 write!(f, "\\x{:02x}", b)?;
                             }
                         }
-                    }
-                    // ASCII control characters except \0, \n, \r, \t
-                    '\x01'..='\x08'
-                    | '\x0b'
-                    | '\x0c'
-                    | '\x0e'..='\x19'
-                    | '\x7f' => {
-                        write!(f, "\\x{:02x}", ch as u32)?;
-                    }
-                    '\n' | '\r' | '\t' => {
-                        write!(f, "{}", ch.escape_debug())?;
                     }
                     _ => {
                         write!(f, "{}", ch.escape_debug())?;
@@ -1305,7 +1297,12 @@ fn test_debug() {
         // Before fixing #188, the output here would be:
         //   \\xED\\xA0\\x80Aa\\x7f\\x0b
         B(&format!("{:?}", b"\xed\xa0\x80Aa\x7f\x0b".as_bstr())).as_bstr(),
-    )
+    );
+
+    assert_eq!(
+        r#""\0\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x11\x12\r\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f \x7f\x80\x81\xfe\xff""#,
+        format!("{:?}", b"\0\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x11\x12\r\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f \x7f\x80\x81\xfe\xff".as_bstr()),
+    );
 }
 
 // See: https://github.com/BurntSushi/bstr/issues/82
